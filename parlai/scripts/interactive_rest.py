a78eba5e13c2f043c9924ef2e2ccc40372854bb5
jesusdelpeso <df382ef8615a9b3fdd2d1343267c3f14edce82d8>
2022-11-14 14:05:07 +0100

    [[fix]] Add test web interface.

--- parlai/scripts/interactive_rest.py
+++ parlai/scripts/interactive_rest.py
@@ -30,6 +30,149 @@ from parlai.agents.local_human.local_human import LocalHumanAgent
 HOST_NAME = 'localhost'
 PORT = 8080
 
+STYLE_SHEET = "https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.css"
+FONT_AWESOME = "https://use.fontawesome.com/releases/v5.3.1/js/all.js"
+WEB_HTML = """
+<html>
+    <link rel="stylesheet" href={} />
+    <script defer src={}></script>
+    <head><title> Interactive Run </title></head>
+    <body>
+        <div class="columns" style="height: 100%">
+            <div class="column is-three-fifths is-offset-one-fifth">
+              <section class="hero is-info is-large has-background-light has-text-grey-dark" style="height: 100%">
+                <div id="parent" class="hero-body" style="overflow: auto; height: calc(100% - 76px); padding-top: 1em; padding-bottom: 0;">
+                    <article class="media">
+                      <div class="media-content">
+                        <div class="content">
+                          <p>
+                            <strong>Instructions</strong>
+                            <br>
+                            Enter a message, and the model will respond interactively.
+                          </p>
+                        </div>
+                      </div>
+                    </article>
+                </div>
+                <div class="hero-foot column is-three-fifths is-offset-one-fifth" style="height: 76px">
+                  <form id = "interact">
+                      <div class="field is-grouped">
+                        <p class="control is-expanded">
+                          <input class="input" type="text" id="userIn" placeholder="Type in a message">
+                        </p>
+                        <p class="control">
+                          <button id="respond" type="submit" class="button has-text-white-ter has-background-grey-dark">
+                            Submit
+                          </button>
+                        </p>
+                        <p class="control">
+                          <button id="restart" type="reset" class="button has-text-white-ter has-background-grey-dark">
+                            Save and restart Conversation
+                          </button>
+                        </p>
+                      </div>
+                  </form>
+                </div>
+              </section>
+            </div>
+        </div>
+
+        <script>
+			var conversation_id=new Date().valueOf().toString();
+			
+            function createChatRow(agent, text) {{
+                var article = document.createElement("article");
+                article.className = "media"
+
+                var figure = document.createElement("figure");
+                figure.className = "media-left";
+
+                var span = document.createElement("span");
+                span.className = "icon is-large";
+
+                var icon = document.createElement("i");
+                icon.className = "fas fas fa-2x" + (agent === "You" ? " fa-user " : agent === "Model" ? " fa-robot" : "");
+
+                var media = document.createElement("div");
+                media.className = "media-content";
+
+                var content = document.createElement("div");
+                content.className = "content";
+
+                var para = document.createElement("p");
+                var paraText = document.createTextNode(text);
+
+                var strong = document.createElement("strong");
+                strong.innerHTML = agent;
+                var br = document.createElement("br");
+
+                para.appendChild(strong);
+                para.appendChild(br);
+                para.appendChild(paraText);
+                content.appendChild(para);
+                media.appendChild(content);
+
+                span.appendChild(icon);
+                figure.appendChild(span);
+
+                if (agent !== "Instructions") {{
+                    article.appendChild(figure);
+                }};
+
+                article.appendChild(media);
+
+                return article;
+            }}
+            document.getElementById("interact").addEventListener("submit", function(event){{
+                event.preventDefault()
+                var text = document.getElementById("userIn").value;
+				var request = {{"text": text, "conversation_id": conversation_id}};
+                document.getElementById('userIn').value = "";
+
+                fetch('/interact', {{
+                    headers: {{
+                        'Content-Type': 'application/json'
+                    }},
+                    method: 'POST',
+                    body: JSON.stringify(request),
+                }}).then(response=>response.json()).then(data=>{{
+                    var parDiv = document.getElementById("parent");
+
+                    parDiv.append(createChatRow("You", text));
+
+                    // Change info for Model response
+                    parDiv.append(createChatRow("Model", data.text));
+                    parDiv.scrollTo(0, parDiv.scrollHeight);
+                }})
+            }});
+            document.getElementById("interact").addEventListener("reset", function(event){{
+                event.preventDefault()
+                var text = document.getElementById("userIn").value;
+                document.getElementById('userIn').value = "";
+				var request = {{"conversation_id": conversation_id}};
+
+                fetch('/reset', {{
+                    headers: {{
+                        'Content-Type': 'application/json'
+                    }},
+                    method: 'POST',
+					body: JSON.stringify(request)
+                }}).then(response=>response.json()).then(data=>{{
+                    var parDiv = document.getElementById("parent");
+
+                    parDiv.innerHTML = '';
+                    parDiv.append(createChatRow("Instructions", "Enter a message, and the model will respond interactively."));
+                    parDiv.scrollTo(0, parDiv.scrollHeight);
+                }})
+				conversation_id=new Date().valueOf().toString();
+            }});
+        </script>
+
+    </body>
+</html>
+"""  # noqa: E501
+
+
 SHARED: Dict[Any, Any] = {}
 
 
@@ -137,6 +280,30 @@ class MyHandler(BaseHTTPRequestHandler):
         world_logger = SHARED["active_conversations"][conversation_id]["world_logger"]
         world_logger.reset_world()
         world_logger.write(out_file, SHARED["active_conversations"][conversation_id]["world"], save_format)
+
+    def do_GET(self):
+        """
+        Respond to GET request, especially the initial load.
+        """
+        paths = {
+            '/': {'status': 200},
+            '/favicon.ico': {'status': 202},  # Need for chrome
+        }
+        if self.path in paths:
+            self._respond(paths[self.path])
+        else:
+            self._respond({'status': 500})
+
+    def _respond(self, opts):
+        response = self._handle_http(opts['status'], self.path)
+        self.wfile.write(response)
+
+    def _handle_http(self, status_code, path, text=None):
+        self.send_response(status_code)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        content = WEB_HTML.format(STYLE_SHEET, FONT_AWESOME)
+        return bytes(content, 'UTF-8')
 
 
 def setup_inter_rest_args(shared):
